6926a8773646bb8f3f4923045e4c4debc6dab868
anagno <anagnwstopoulos@hotmail.com>
2019-07-13 14:10:17 +0200

    Updating the travis and the Dockerfile to build the cfssl library
    This is necessary because the binary downloaded was a x86_64 and
    it did not work on different arch images.

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -1,33 +1,4 @@
 #!/bin/bash -e
 
-# download curl and ca-certificate from apt-get if needed
-to_install=()
-
-if [ "$(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
-    to_install+=("curl")
-fi
-
-if [ "$(dpkg-query -W -f='${Status}' ca-certificates 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
-    to_install+=("ca-certificates")
-fi
-
-if [ ${#to_install[@]} -ne 0 ]; then
-    LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends "${to_install[@]}"
-fi
-
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq
-
-echo "Download cfssl ..."
-curl -o /usr/sbin/cfssl -SL https://github.com/osixia/cfssl/releases/download/1.3.3/cfssl_linux-amd64
-chmod 700 /usr/sbin/cfssl
-
-echo "Download cfssljson ..."
-curl -o /usr/sbin/cfssljson -SL https://github.com/osixia/cfssl/releases/download/1.3.3/cfssljson_linux-amd64
-chmod 700 /usr/sbin/cfssljson
-
-echo "Project sources: https://github.com/cloudflare/cfssl"
-
-# remove tools installed to download cfssl
-if [ ${#to_install[@]} -ne 0 ]; then
-    apt-get remove -y --purge --auto-remove "${to_install[@]}"
-fi
+# No downloads are necessary because the cfssl is being download
+# and build during the creation of the image
